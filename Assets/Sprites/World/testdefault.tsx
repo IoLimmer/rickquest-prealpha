93a06a9d48c436ff23c1d3d8c6352fb0d12891c6
IoLimmer <jh20006@bristol.ac.uk>
2023-04-22 21:47:47 +0100

    Created test background

---
+++ Assets/Sprites/World/testdefault.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="testdefault" tilewidth="24" tileheight="24" tilecount="12" columns="6">
+ <image source="testgroundtileset.png" width="144" height="48"/>
+</tileset>
