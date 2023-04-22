f5fe19b32281c9a5986745985b9d16b8982fb771
IoLimmer <jh20006@bristol.ac.uk>
2023-04-22 22:36:24 +0100

    Player controller script - added speed control

--- Assets/Scripts/PlayerControllerGRID.cs
+++ Assets/Scripts/PlayerControllerGRID.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerControllerGRID : MonoBehaviour
 {
+    public float playerSpeed = 0.2f;
+
+    private float timeCounter = 0f;
+    private Vector3 change;
+    public bool canMove = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 change = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
+
+        if (canMove) {
+            transform.Translate(change);
+            canMove = false;
+        }
+
+        if (timeCounter > playerSpeed)
+        {
+            canMove = true;
+            timeCounter = 0f;
+        }
+
+        timeCounter += Time.deltaTime;
+        Debug.Log(timeCounter);
     }
 }
