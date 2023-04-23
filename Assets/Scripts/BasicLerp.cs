dca41fe94144f9fc92ff2d12acaf6d57f7c26a24
IoLimmer <jh20006@bristol.ac.uk>
2023-04-23 16:53:45 +0100

    Attempted lerping sprite

--- Assets/Scripts/BasicLerp.cs
+++ Assets/Scripts/BasicLerp.cs
@@ -6,6 +6,7 @@ public class BasicLerp : MonoBehaviour
 {
     [SerializeField] private Vector3 parentPositionOld;
     [SerializeField] private Vector3 parentPositionNew;
+    [SerializeField] private Vector3 lerpPosition;
     [SerializeField] private float lerpSpeed = 0.5f;
 
     // Start is called before the first frame update
@@ -17,8 +18,16 @@ public class BasicLerp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        parentPositionOld = gameObject.transform.parent.position + new Vector3(3f,3f,0f);
+        parentPositionNew = gameObject.transform.parent.position;
 
-        gameObject.transform.position = parentPositionOld;
+        if (parentPositionNew != parentPositionOld)
+        {
+            Debug.Log("banana");
+            lerpPosition = Vector3.Lerp(parentPositionOld, parentPositionNew, lerpSpeed);
+            gameObject.transform.position = lerpPosition;
+        }
+        //parentPositionOld = gameObject.transform.parent.position + new Vector3(3f,3f,0f);
+
+        //gameObject.transform.position = parentPositionOld;
     }
 }
