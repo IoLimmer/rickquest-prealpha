02003500a0b995eb3e233b024c21a8457f35e70b
IoLimmer <jh20006@bristol.ac.uk>
2023-04-24 19:26:11 +0100

    followers follower player!

--- Assets/Scripts/FollowerController.cs
+++ Assets/Scripts/FollowerController.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class FollowerController : MonoBehaviour
 {
-    private int positionInCaterpillar;
+    public float moveSpeed = 5f;
+    [SerializeField] private List<Vector3> followerTargets;
+    public int positionInCaterpillar;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        followerTargets = gameObject.GetComponentInParent<CaterpillarManager>().followerTargets;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, followerTargets[positionInCaterpillar], moveSpeed * Time.deltaTime);
+
+        followerTargets = gameObject.GetComponentInParent<CaterpillarManager>().followerTargets;
     }
 }
