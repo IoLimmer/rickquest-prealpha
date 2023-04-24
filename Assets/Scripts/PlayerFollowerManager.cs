3adcfd62ab9ab18abf883ad5e4e15febd9e09182
IoLimmer <jh20006@bristol.ac.uk>
2023-04-24 15:57:21 +0100

    player follower manager spawns in player at given location

--- Assets/Scripts/PlayerFollowerManager.cs
+++ Assets/Scripts/PlayerFollowerManager.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class PlayerFollowerManager : MonoBehaviour
 {
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private Vector3 playerOrigin;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerOrigin += new Vector3(0.5f, 0.5f, 0f);
+        // player origin is written in by dev as 3 whole numbers. arbitrary adjustments to real grid position are added by code above
+        // (it just seemed nicer that way)
+
+        GameObject.Instantiate(playerPrefab, playerOrigin, new Quaternion());
     }
 
     // Update is called once per frame
