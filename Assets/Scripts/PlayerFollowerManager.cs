e219a19c90750b7d0c148d40bda154246c429b18
IoLimmer <jh20006@bristol.ac.uk>
2023-04-24 16:13:41 +0100

    created followercount, enum describing where to spawn followers relative to player, and list for follower targets

--- Assets/Scripts/PlayerFollowerManager.cs
+++ Assets/Scripts/PlayerFollowerManager.cs
@@ -6,6 +6,18 @@ public class PlayerFollowerManager : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab;
     [SerializeField] private Vector3 playerOrigin;
+    enum FollowerOriginDirection
+    {
+        Behind_player,
+        In_front_of_player,
+        Left_of_player,
+        Right_of_player
+    };
+
+    [SerializeField] private FollowerOriginDirection followerOriginDirection;
+
+    [SerializeField] private int followerCount = 3;
+    [SerializeField] public List<Vector3> followerTargets;
 
     // Start is called before the first frame update
     void Start()
