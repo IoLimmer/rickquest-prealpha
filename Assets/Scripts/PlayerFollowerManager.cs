a79d59f18a836e7a6516cd606d23f1be90e291ed
IoLimmer <jh20006@bristol.ac.uk>
2023-04-24 16:28:48 +0100

    manager successfully spawned one follower behind player

--- Assets/Scripts/PlayerFollowerManager.cs
+++ Assets/Scripts/PlayerFollowerManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerFollowerManager : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private GameObject followerPrefab;
     [SerializeField] private Vector3 playerOrigin;
     enum FollowerOriginDirection
     {
@@ -16,8 +17,34 @@ public class PlayerFollowerManager : MonoBehaviour
 
     [SerializeField] private FollowerOriginDirection followerOriginDirection;
 
-    [SerializeField] private int followerCount = 3;
+    [SerializeField] private int followerCount = 1;
     [SerializeField] public List<Vector3> followerTargets;
+
+    void InitFollowerPositions()
+    {
+        switch (followerOriginDirection)
+        {
+            case FollowerOriginDirection.Behind_player:
+                followerTargets.Add(playerOrigin + new Vector3(0f, 1f, 0f));
+                break;
+
+            case FollowerOriginDirection.In_front_of_player:
+                followerTargets.Add(playerOrigin + new Vector3(0f, -1f, 0f));
+                break;
+
+            case FollowerOriginDirection.Left_of_player:
+                followerTargets.Add(playerOrigin + new Vector3(-1f, 0f, 0f));
+                break;
+
+            case FollowerOriginDirection.Right_of_player:
+                followerTargets.Add(playerOrigin + new Vector3(1f, 0f, 0f));
+                break;
+
+            default:
+                Debug.LogError("Follower spawn location not defined!");
+                break;            
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -25,8 +52,10 @@ public class PlayerFollowerManager : MonoBehaviour
         playerOrigin += new Vector3(0.5f, 0.5f, 0f);
         // player origin is written in by dev as 3 whole numbers. arbitrary adjustments to real grid position are added by code above
         // (it just seemed nicer that way)
+        InitFollowerPositions();
 
         GameObject.Instantiate(playerPrefab, playerOrigin, new Quaternion());
+        GameObject.Instantiate(followerPrefab, followerTargets[0], new Quaternion());
     }
 
     // Update is called once per frame
